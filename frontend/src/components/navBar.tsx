fe768094b3eb32b20bb4554228c2b2f310056043
Gabriel Tramontin <gabrieljoaquim080706@gmail.com>
2025-06-20 19:04:02 -0300

    trocando a tag a por link

--- frontend/src/components/navBar.tsx
+++ frontend/src/components/navBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
     <nav className="bg-white shadow-2xs w-full">
       <div className="flex items-center justify-between h-20 px-4 md:px-10">
         <div className="mt-3 w-full flex justify-center md:justify-start">
-          <a href="/"><Image src="/logo.png" alt="logo" width={160} height={80} /></a>
+          <Link href="/"><Image src="/logo.png" alt="logo" width={160} height={80} /></Link>
         </div>
 
         <div className="md:hidden">
@@ -22,20 +22,20 @@ export default function NavBar() {
         </div>
 
         <ul className="hidden md:flex gap-8">
-          <li><a href="/" className="hover:text-orange-600">Home</a></li>
-          <li><a href="#contact" className="hover:text-orange-600">Contact</a></li>
-          <li><a href="#about" className="hover:text-orange-600">About</a></li>
-          <li><a href="#services" className="hover:text-orange-600">Services</a></li>
+          <li><Link href="/" className="hover:text-orange-600">Home</Link></li>
+          <li><Link href="#contact" className="hover:text-orange-600">Contact</Link></li>
+          <li><Link href="#about" className="hover:text-orange-600">About</Link></li>
+          <li><Link href="#services" className="hover:text-orange-600">Services</Link></li>
         </ul>
       </div>
 
       {/* Menu - Mobile */}
       {isOpen && (
         <ul className="md:hidden flex flex-col items-center gap-4 pb-4">
-          <li><a href="/" className="hover:text-orange-600">Home</a></li>
-          <li><a href="#contact" className="hover:text-orange-600">Contact</a></li>
-          <li><a href="#About" className="hover:text-orange-600">About</a></li>
-          <li><a href="#services" className="hover:text-orange-600">Services</a></li>
+          <li><Link href="/" className="hover:text-orange-600">Home</Link></li>
+          <li><Link href="#contact" className="hover:text-orange-600">Contact</Link></li>
+          <li><Link href="#About" className="hover:text-orange-600">About</Link></li>
+          <li><Link href="#services" className="hover:text-orange-600">Services</Link></li>
         </ul>
       )}
     </nav>
